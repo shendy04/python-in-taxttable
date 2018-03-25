c318c179fbf0c1e883cedc36b142bc2910b0a5cc
shendypatikasari <37423006+shendy04@users.noreply.github.com>
2018-03-25 01:15:38 -0700

    Add files via upload

---
+++ sendipatikasari.py
@@ -0,0 +1,24 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:16:31) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Masukan Nama : nova
+Masukan NIM : 6767
+Nilai Tugas : 80
+Nilai UTS : 90
+Nilai UAS : 90
+Tambah Data (y/n)?y
+Masukan Nama : dandi
+Masukan NIM : 9090
+Nilai Tugas : 80
+Nilai UTS : 89
+Nilai UAS : 89
+Tambah Data (y/n)?t
++----+-------+------+-------------+-----------+-----------+
+| No | Nama  | NIM  | Nilai Tugas | Nilai UTS | Nilai UAS |
++====+=======+======+=============+===========+===========+
+| 1  | nova  | 6767 | 80          | 90        | 90        |
++----+-------+------+-------------+-----------+-----------+
+| 2  | dandi | 9090 | 80          | 89        | 89        |
++----+-------+------+-------------+-----------+-----------+
+>>> 
